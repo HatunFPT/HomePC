d06002efbd9492fc3844bf79bd4b69b9dbeccce2
Hatun <hatun.fpt@gmail.com>
2023-11-04 14:13:43 +0700

    put

---
+++ LMS_COM108/ASM/test1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	//ý 1
+	int a;
+	float b;
+	bool c;
+	char d;
+	//ý 2
+	int a1=7;
+	//ý 3
+	float b1=5.5;
+	
+	return 0;
+}
+
+
+
